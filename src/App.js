075becb2921738504d51de1d1a668224f412ab59
pieceofchum <pieceofchum@yahoo.com>
2019-02-14 20:19:20 -0800

    change app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello There Dragon!!!!!
+            Hello There Dragon Lord - Feature!!!!!
           </p>
           <a
             className="App-link"
